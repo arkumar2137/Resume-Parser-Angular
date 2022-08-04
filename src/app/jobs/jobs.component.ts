1699ec7adb0697d8b514e4eefc481c77d5b9d928
arkumar2137 <arkumar2137@gmail.com>
2022-08-04 11:27:33 +0530

    added routes for JD

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -11,13 +11,41 @@ import { CanditatesDetailService } from '../canditates-detail.service';
 })
 export class JobsComponent implements OnInit {
 
-  elements = [];
+  elements:any = [];
+  profileForm:any = FormGroup;
 
-  constructor() { }
+  name: string = '';
+  files: any;
+  pdfFile: any;
+  result: any;
+
+  getTheInput(name: string){
+    this.name = name;
+  }
+
+  getTheFile(event: any){
+    this.files = event.target.files[0];
+    console.log('file', this.files);
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     
   }
 
+  onSubmit(){
+    console.log('button pressed');
+    let formData = new FormData();
+    formData.set("name", this.name);
+    formData.set("jd_file", this.files);
+
+    this.http.post('http://localhost:5000/uploads_jd', formData).subscribe(
+      resp => {
+        this.elements = resp;
+        console.log(this.elements)
+      }
+    )
+  }
 
 }
